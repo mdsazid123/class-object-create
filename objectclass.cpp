ef3666c7bf27d923c613721f4a628d5e674ad68a
Md.Sazid <115614150+mdsazid123@users.noreply.github.com>
2024-01-13 00:03:09 +0530

    easy to create class&object

---
+++ objectclass.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+class Sazid{
+    public:
+    string  name;
+    int  b= 25361;
+
+};
+int main()
+{
+    int a;
+    string name= "sazid";
+    cout<<"b"<<endl;
+    cout<<"sazid";
+};
+
+
